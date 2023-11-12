8a2d868e74fcf37282640d3b22d5c965d43b2d4e
CrackMiinD <96918247+CrackMiinD@users.noreply.github.com>
2023-11-12 14:21:22 +0200

    a

--- app/api/resend/route.ts
+++ app/api/resend/route.ts
@@ -28,3 +28,8 @@
 //     return NextResponse.json({ error });
 //   }
 // }
+import { NextResponse } from "next/server";
+export async function GET(request: Request) {
+  return NextResponse.json({ message: "Hello, Next.js!" });
+}
+ 
