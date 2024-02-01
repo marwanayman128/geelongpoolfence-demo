f3fc91001aaa344f7ba3272642c222ff7a860a20
Marwan Ayman Mohamed <marwan_ayman86@yahoo.com>
2024-02-01 07:19:15 +0200

    gtag

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,9 +1,31 @@
 "use client";
 import Home from "@mui/icons-material/Home";
+import Head from "next/head";
 
 const ThankYouPage = () => {
   return (
     <>
+      <Head>
+        {/* Google Analytics script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11412063683"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11412063683');
+            gtag('config', 'AW-11412063683/wBg4CMGT4vYYEMOL2cEq', {
+              'phone_conversion_number': '0404 494 904'
+            });
+            gtag('event', 'conversion', {'send_to': 'AW-11412063683/-n9ZCO7e3fYYEMOL2cEq'});
+          `,
+          }}
+        />
+      </Head>
       <div className="relative  h-[90vh]  w-full overflow-hidden ">
         <video
           loop
