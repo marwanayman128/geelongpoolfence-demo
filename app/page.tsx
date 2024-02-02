70a61a43dceaef1aa8dc931c7c81f4a53f8258b5
Marwan Ayman Mohamed <96918247+marwanayman128@users.noreply.github.com>
2024-02-02 04:43:28 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,34 @@ import ScrollToTop from "@/app/components/scrollToTop/ScrollToTop";
 export default function Home() {
   return (
     <>
-   
+   <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11412063683"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(...args: any[]): void {
+                window.dataLayer.push(args);
+              }
+              gtag('js', new Date());
+              gtag('config', 'AW-11412063683');
+            `,
+          }}
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              gtag('config', 'AW-11412063683/wBg4CMGT4vYYEMOL2cEq', {
+                'phone_conversion_number': '0404 494 904'
+              });
+            `,
+          }}
+        ></script>
+      </head>
       <Section1 />
       <Section2 />
       <Section4 />
