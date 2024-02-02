b2b30c37808c9901c986751e91f4653b32647d6c
Marwan Ayman Mohamed <96918247+marwanayman128@users.noreply.github.com>
2024-02-02 11:48:27 +0200

    Add files via upload

---
+++ app/script.tsx
@@ -0,0 +1,15 @@
+// pages/page2.js
+"use client";
+import { usePathname } from "next/navigation";
+import GoogleTag from "./api/google-tag.jsx";
+import GoogleTagThankYou from "./api/google-tag-thankyou.jsx";
+
+const Page2 = () => {
+  const pathname = usePathname();
+
+  const isThankYouPage = pathname === "/thankyou";
+
+  return <head>{isThankYouPage ? <GoogleTagThankYou /> : <GoogleTag />}</head>;
+};
+
+export default Page2;
