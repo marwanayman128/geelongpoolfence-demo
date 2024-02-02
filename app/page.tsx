54592648d9ffaaaccac3646371c9bd9e544ade6b
Marwan Ayman Mohamed <96918247+marwanayman128@users.noreply.github.com>
2024-02-02 03:49:15 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,28 @@ import Section4 from "@/app/components/sections/section4";
 import Section5 from "@/app/components/sections/section5";
 import Contact from "@/app/components/sections/contactSection";
 import ScrollToTop from "@/app/components/scrollToTop/ScrollToTop";
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <>
+      <Head>
+        {/* Google Analytics gtag.js script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11412063683"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11412063683');
+
+            gtag('config', 'AW-11412063683/wBg4CMGT4vYYEMOL2cEq', {
+              'phone_conversion_number': '0404 494 904'
+            });
+          `}
+        </script>
+
+      </Head>
       <Section1 />
       <Section2 />
       <Section4 />
